29dd570f67cb4b53410c61c64510481cc5d84f68
Ken Cheng <zerochrono@gmail.com>
2014-10-06 23:32:59 -0700

    Change licenses to CC BY-NC-SA

--- src/LikelihoodSolver/NoSuspectTwoUnknownsLikelihoodSolver.cpp
+++ src/LikelihoodSolver/NoSuspectTwoUnknownsLikelihoodSolver.cpp
@@ -1,12 +1,9 @@
 //==================================================================================================
 // Name        : NoSuspectTwoUnknownsLikelihoodSolver.cpp
 // Author      : Ken Cheng
-// Copyright   : This program is free software: you can redistribute it and/or modify it under the
-//   terms of the GNU General Public License as published by the Free Software Foundation, either
-//   version 3 of the License, or (at your option) any later version.  This program is distributed
-//   in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-//   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-//   more details see <http://www.gnu.org/licenses/>.
+// Copyright   : This work is licensed under the Creative Commons
+//     Attribution-NonCommercial-ShareAlike 4.0 International License. To view a copy of this
+//     license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
 // Description : 
 //==================================================================================================
 
